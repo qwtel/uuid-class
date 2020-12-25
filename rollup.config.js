2dedf6b80484ad580633747e5c36632b26cc3063
Florian Klampfer <mail@qwtel.com>
2020-12-25 10:18:32 +0700

    Fix module

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,7 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-// import { uglify } from "rollup-plugin-uglify";
 
 export default [
-  // {
-  //   input: 'cjs/index.cjs',
-  //   output: {
-  //     file: `umd/index.js`,
-  //     format: 'umd',
-  //     name: 'base64Encoding',
-  //     sourcemap: true
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     commonjs(),
-  //     uglify(),
-  //   ],
-  // }, 
   {
     input: 'index.js',
     output: {
@@ -25,6 +9,6 @@ export default [
       format: 'es',
       sourcemap: true
     },
-    plugins: [terser()],
+    plugins: [resolve(), terser()],
   }
 ];
